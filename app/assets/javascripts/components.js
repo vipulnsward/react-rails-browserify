75b72341b594d57748426cbabc5302e31a069b53
Vipul A M <vipulnsward@gmail.com>
2015-03-14 19:42:25 +0530

    Init

---
+++ app/assets/javascripts/components.js
@@ -0,0 +1,9 @@
+//= require_tree ./components
+//= require_self
+//= require react_ujs
+
+
+React = require('react/addons');
+
+DemoComponent = require('./components/DemoComponent');
+TodoApp = require('./components/TodoApp');
---
+++ app/assets/javascripts/components/DemoComponent.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var DemoComponent = React.createClass({displayName: 'Demo Component',
+    render: function() {
+        return <div>Demo Component</div>;
+    }
+});
+
+
+// each file will export exactly one component
+module.exports = DemoComponent;
